b6cb0934a922c6ce7fb77a9d59e72add504ab0ec
daikien <daikien@icloud.com>
2017-10-07 01:37:49 +0200

    let's get started

--- nocv_web/src/App.js
+++ nocv_web/src/App.js
@@ -20,13 +20,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          <Start />
-        </p>
+        <Start />
       </div>
     );
   }
--- nocv_web/src/pages/Start.js
+++ nocv_web/src/pages/Start.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
-import { Navbar, Jumbotron, Button } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 
 export const Start = () => {
   return (
     <div>
       <div>
-
+        <img src='Path to img' alt="logo" />
       </div>
       <div>
-        <Button>Hire</Button>
-        <Button>Apply</Button>
+        <Button className = "btn btn-primary btn-lg">Hire</Button>
+        <Button className="btn-lg">Apply</Button>
+
+          <div>
+              <style type="text/css">{'.btn-custom { background-color: "#f4f4f4"; color: white; }'}</style>
+              <Button bsStyle="custom">Custom</Button>
+            </div>
+
       </div>
     </div>
   );
