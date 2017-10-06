7b7d4e2f8ab692ee8683ae873a29eb108259c7ea
guseberjo <guseberjo@student.gu.se>
2017-10-07 00:39:34 +0200

    done

--- nocv_web/src/pages/Start.js
+++ nocv_web/src/pages/Start.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react'
-import { Button, Image } from 'react-bootstrap'
+import React, { Component } from 'react';
+import { Navbar, Jumbotron, Button } from 'react-bootstrap';
 
 export const Start = () => {
   return (
     <div>
       <div>
-        <Image />
+
       </div>
       <div>
         <Button>Hire</Button>
